9020418ae9b51b4cd20a1a59eeb63ecf97136439
OM MAHALE <78585854+ommahale@users.noreply.github.com>
2023-03-15 06:19:49 +0530

    add modules

---
+++ lib/screens/loading.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoadinScreen extends StatefulWidget {
+  const LoadinScreen({super.key});
+
+  @override
+  State<LoadinScreen> createState() => _LoadinScreenState();
+}
+
+class _LoadinScreenState extends State<LoadinScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
